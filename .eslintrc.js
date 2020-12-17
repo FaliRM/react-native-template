a1780487f728137688bc04009cc1e4491a4388d4
Rafael <rafael+1@bolt.global>
2020-12-17 18:09:19 +0800

    change the ts and linters config
    change the ts and linters config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  env: {
+    node: true,
+    es6: true,
+    browser: true,
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 2,
+    'react/prop-types': 0,
+    'react/display-name': 0,
+  },
 };
